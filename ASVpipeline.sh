07dc831c7abe8e8e2bd60ec7a6b0583aed521dfc
kasperskytte <knaldhat@gmail.com>
2019-01-10 11:32:28 +0100

    unquote paths, white space in path must be escaped

--- ASVpipeline.sh
+++ ASVpipeline.sh
@@ -3,9 +3,9 @@
 set -e
 set -o pipefail
 
-NUMTHREADS=${1:-$((`nproc`-2))}
-SEQPATH="/home/kapper/dragun/space/sequences/"
-TAXDB="/home/kapper/Dropbox/AAU/PhD/Projects/ESV pipeline/runs/MiDAS3.0/midas30_20180108/output/ESVs_w_sintax.fa"
+MAX_THREADS=${1:-$((`nproc`-2))}
+SEQPATH=/home/kapper/dragun/space/sequences/
+TAXDB=/home/kapper/Dropbox/AAU/PhD/Projects/ESV\ pipeline/runs/MiDAS3.0/midas30_20190109/output/ESVs_w_sintax.fa
 SAMPLESEP="_"
 
 rm -rf rawdata/
@@ -17,15 +17,16 @@ echoWithDate() {
   echo "[$(date '+%Y-%m-%d %H:%M:%S')]: $1"
 }
 
+echoWithDate "Running ASV pipeline (max threads: $MAX_THREADS)..."
 echoWithDate "Finding samples and filtering PhiX from reads (forward only)..."
 cat samples | tr "\r" "\n" | sed -e '$a\' | sed -e '/^$/d' -e 's/ //g' > samples_tmp.txt
 
 NSAMPLES=$(wc -w < samples_tmp.txt)
 while ((i++)); read SAMPLE
   do
-  echo -ne "Processing sample $SAMPLE ($i / $NSAMPLES)\r"
-  find $SEQPATH -name $SAMPLE$SAMPLESEP*R1* 2>/dev/null -exec gzip -cd {} \; > rawdata/$SAMPLE.R1.fq
-  usearch10 -filter_phix rawdata/$SAMPLE.R1.fq -output phix_filtered/$SAMPLE.R1.fq -threads $NUMTHREADS -quiet
+  echo -ne "Processing sample: $SAMPLE ($i / $NSAMPLES)\r"
+  find "$SEQPATH" -name $SAMPLE$SAMPLESEP*R1* 2>/dev/null -exec gzip -cd {} \; > rawdata/$SAMPLE.R1.fq
+  usearch10 -filter_phix rawdata/$SAMPLE.R1.fq -output phix_filtered/$SAMPLE.R1.fq -threads $MAX_THREADS -quiet
   rm rawdata/$SAMPLE.R1.fq
 done < samples_tmp.txt
 
@@ -34,8 +35,8 @@ mkdir phix_filtered/tempdir
 # merge all sample fastq files
 while ((j++)); read SAMPLE
   do
-  echo -ne "Processing sample $SAMPLE ($j / $NSAMPLES)\r"
-  usearch10 -fastq_filter phix_filtered/$SAMPLE.R1.fq -fastq_maxee 1.0 -fastaout phix_filtered/tempdir/$SAMPLE.R1.QCout.fa -fastq_trunclen 250 -relabel @ -threads $NUMTHREADS -quiet
+  echo -ne "Processing sample: $SAMPLE ($j / $NSAMPLES)\r"
+  usearch10 -fastq_filter phix_filtered/$SAMPLE.R1.fq -fastq_maxee 1.0 -fastaout phix_filtered/tempdir/$SAMPLE.R1.QCout.fa -fastq_trunclen 250 -relabel @ -threads $MAX_THREADS -quiet
   cat phix_filtered/tempdir/$SAMPLE.R1.QCout.fa >> all.singlereads.nophix.qc.R1.fa
 
   # Create concatenated fastq file of nonfiltered reads, with the sample labels
@@ -51,9 +52,10 @@ usearch10 -unoise3 uniques.R1.fa -zotus ASVs.R1.fa -quiet
 sed -i 's/Zotu/ASV/g' ASVs.R1.fa
 
 echoWithDate "Predicting taxonomy the zOTUs..."
-usearch10 -sintax ASVs.R1.fa -db $TAXDB -tabbedout ASVs.R1.sintax -strand both -sintax_cutoff 0.8 -threads $NUMTHREADS -quiet
+usearch10 -sintax ASVs.R1.fa -db "$TAXDB" -tabbedout ASVs.R1.sintax -strand both -sintax_cutoff 0.8 -threads $MAX_THREADS -quiet
 sort -V ASVs.R1.sintax > ASVs.R1.sorted.sintax
 
+echoWithDate "Adding predicted taxonomy to the zOTU FASTA headers..."
 R --slave << 'sintaxtofastaheader'
   suppressPackageStartupMessages({
     #Biostrings (and BiocManager which is used to install Biostrings)
@@ -64,7 +66,7 @@ R --slave << 'sintaxtofastaheader'
       BiocManager::install("Biostrings", update = FALSE, ask = FALSE)
     }
     if(!require("Biostrings"))
-      stop("The Biostrings R package is not available, skipping step", call. = FALSE)
+      stop("   The Biostrings R package is not available, skipping step", call. = FALSE)
     
     #dplyr
     if(!require("dplyr")) {
