7260c9b0695477a099adee4eecf1145aeb8e4f32
Kasper Skytte Andersen <knaldhat@gmail.com>
2019-06-17 17:15:45 +0200

    run usearch from variable to allow different versions

--- ASVpipeline.sh
+++ ASVpipeline.sh
@@ -3,6 +3,7 @@
 set -e
 set -o pipefail
 
+usearch=$(which usearch11_32bit)
 MAX_THREADS=${1:-$((`nproc`-2))}
 SEQPATH=/home/kapper/dragun/space/sequences/
 TAXDB=/home/kapper/Dropbox/AAU/PhD/Projects/ESV\ pipeline/runs/MiDAS4.0_20190524/output/ESVs_w_sintax.fa
@@ -33,19 +34,19 @@ while ((i++)); read SAMPLE
     if [ -s "rawdata/$SAMPLE.R1.fq" ]
       then
         #filter PhiX
-        usearch10 -filter_phix rawdata/$SAMPLE.R1.fq -output phix_filtered/$SAMPLE.R1.fq -threads $MAX_THREADS -quiet
+        $usearch -filter_phix rawdata/$SAMPLE.R1.fq -output phix_filtered/$SAMPLE.R1.fq -threads $MAX_THREADS -quiet
         rm rawdata/$SAMPLE.R1.fq
         
         #QC
         if [ -s "phix_filtered/$SAMPLE.R1.fq" ]
           then
-            usearch10 -fastq_filter phix_filtered/$SAMPLE.R1.fq -fastq_maxee 1.0 -fastaout phix_filtered/tempdir/$SAMPLE.R1.QCout.fa \
+            $usearch -fastq_filter phix_filtered/$SAMPLE.R1.fq -fastq_maxee 1.0 -fastaout phix_filtered/tempdir/$SAMPLE.R1.QCout.fa \
               -fastq_trunclen 250 -relabel @ -threads $MAX_THREADS -quiet
             cat phix_filtered/tempdir/$SAMPLE.R1.QCout.fa >> all.singlereads.nophix.qc.R1.fa
             rm phix_filtered/tempdir/$SAMPLE.R1.QCout.fa
             
             # Create concatenated fastq file of nonfiltered reads, with the sample labels
-            usearch10 -fastx_relabel phix_filtered/$SAMPLE.R1.fq -prefix $SAMPLE$SAMPLESEP -fastqout phix_filtered/tempdir/$SAMPLE.R1.relabeled.fq -quiet
+            $usearch -fastx_relabel phix_filtered/$SAMPLE.R1.fq -prefix $SAMPLE$SAMPLESEP -fastqout phix_filtered/tempdir/$SAMPLE.R1.relabeled.fq -quiet
             cat phix_filtered/tempdir/$SAMPLE.R1.relabeled.fq >> all.singlereads.nophix.R1.fq
             rm phix_filtered/$SAMPLE.R1.fq
         fi
@@ -53,15 +54,15 @@ while ((i++)); read SAMPLE
 done < samples_tmp.txt
 
 echoWithDate "Dereplicating reads..."
-usearch10 -fastx_uniques all.singlereads.nophix.qc.R1.fa -sizeout -fastaout uniques.R1.fa -relabel Uniq -quiet
+$usearch -fastx_uniques all.singlereads.nophix.qc.R1.fa -sizeout -fastaout uniques.R1.fa -relabel Uniq -quiet
 
 echoWithDate "Generating ASVs (zOTUs) from dereplicated reads..."
-usearch10 -unoise3 uniques.R1.fa -zotus zOTUs.R1.fa
+$usearch -unoise3 uniques.R1.fa -zotus zOTUs.R1.fa
 
 echoWithDate "Filtering ASVs that are <60% similar to reference reads..."
 if [ -s "$prefilterDB" ]
   then
-    usearch10 -usearch_global zOTUs.R1.fa -db $prefilterDB \
+    $usearch -usearch_global zOTUs.R1.fa -db $prefilterDB \
       -strand both -id 0.6 -maxaccepts 1 -maxrejects 8 -matched prefilt_out.fa -threads $MAX_THREADS -quiet
     mv prefilt_out.fa zOTUs.R1.fa
   else
@@ -71,9 +72,9 @@ fi
 echoWithDate "Searching ASVs against already known ASVs (exact match) and renaming accordingly..."
 if [ -s "$ASVDB" ]
   then
-    usearch10 -search_exact zOTUs.R1.fa -db $ASVDB -maxaccepts 1 -maxrejects 0 -strand both \
+    $usearch -search_exact zOTUs.R1.fa -db $ASVDB -maxaccepts 1 -maxrejects 0 -strand both \
       -dbmatched ASVs.R1.fa -notmatched ASVs_nohits.R1.fa -threads $MAX_THREADS -quiet
-    usearch10 -fastx_relabel ASVs_nohits.R1.fa -prefix newASV -fastaout ASVs_nohits_renamed.R1.fa -quiet
+    $usearch -fastx_relabel ASVs_nohits.R1.fa -prefix newASV -fastaout ASVs_nohits_renamed.R1.fa -quiet
     #combine hits with nohits
     cat ASVs_nohits_renamed.R1.fa >> ASVs.R1.fa
   else
@@ -84,14 +85,14 @@ fi
 echoWithDate "Predicting taxonomy of the ASVs..."
 if [ -s "$TAXDB" ]
   then
-    usearch10 -sintax ASVs.R1.fa -db "$TAXDB" -tabbedout ASVs.R1.sintax -strand both -sintax_cutoff 0.8 -threads $MAX_THREADS -quiet
+    $usearch -sintax ASVs.R1.fa -db "$TAXDB" -tabbedout ASVs.R1.sintax -strand both -sintax_cutoff 0.8 -threads $MAX_THREADS -quiet
     sort -V ASVs.R1.sintax -o ASVs.R1.sintax
   else
     echo "Could not find taxonomy database, continuing without assigning taxonomy..."    
 fi
 
 echoWithDate "Generating ASV table..."
-usearch10 -otutab all.singlereads.nophix.R1.fq -zotus ASVs.R1.fa -otutabout ASVtable.tsv -threads $MAX_THREADS -sample_delim $SAMPLESEP
+$usearch -otutab all.singlereads.nophix.R1.fq -zotus ASVs.R1.fa -otutabout ASVtable.tsv -threads $MAX_THREADS -sample_delim $SAMPLESEP
 #sort ASVtable
 head -n 1 ASVtable.tsv > tmp
 tail -n +2 ASVtable.tsv | sort -V >> tmp
